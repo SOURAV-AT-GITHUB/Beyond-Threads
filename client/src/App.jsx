ba23cb0cbad1987050c5298f70240edb44358329
Söùrãv <souravpramanick1997@gmail.com>
2025-04-06 14:13:21 +0530

    Routes connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,28 @@
-
-import { Routes,Route } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './Pages/Home/Home'
-import Login from './Pages/Login'
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login";
+import Footer from "./components/Footer";
+import Product from "./Pages/Product";
+import TrendingSarees from "./Pages/TrendingSarees";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
-
+  const location = useLocation();
   return (
     <>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/login' element={<Login/>}/>
-
-  </Routes>
+      {location.pathname !== "/login" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/trending-sarees" element={<TrendingSarees />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      {location.pathname !== "/login" && <Footer />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
