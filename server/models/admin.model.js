af4f306132a1cc820687548692c80d836e12fe5a
Söùrãv <souravpramanick1997@gmail.com>
2025-05-02 11:05:40 +0530

    minor update

--- server/models/admin.model.js
+++ server/models/admin.model.js
@@ -1,12 +1,12 @@
-const pool = require("../config/postgres.config")
+const pool = require("../config/postgres.config");
 
-async function createAdminTable(){
-    const query = `CREATE TABLE IF NOT EXISTS admins (
+async function createAdminTable() {
+  const query = `CREATE TABLE IF NOT EXISTS admin (
     email VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     name TEXT
-    )`
+    )`;
 
-    await pool.query(query)
+  await pool.query(query);
 }
-module.exports = createAdminTable
+module.exports = createAdminTable;
