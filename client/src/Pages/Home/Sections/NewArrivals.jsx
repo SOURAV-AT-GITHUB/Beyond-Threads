7740baf24b2a9cb1dd9636e567fd99a3d541ca59
Söùrãv <souravpramanick1997@gmail.com>
2025-05-02 12:41:55 +0530

    Data fetching by sub_category=new-arrival and limit 4

--- client/src/Pages/Home/Sections/NewArrivals.jsx
+++ client/src/Pages/Home/Sections/NewArrivals.jsx
@@ -1,26 +1,33 @@
 import diamondBullet from "/Images/diamond-bullet.svg";
 import ArrowButton from "../../../components/ArrowButton";
-import img1 from "/Images/Homepage/new-arrivals/img1.jpg";
-import img2 from "/Images/Homepage/new-arrivals/img2.jpg";
-import img3 from "/Images/Homepage/new-arrivals/img3.jpg";
-import img4 from "/Images/Homepage/new-arrivals/img4.jpg";
-import AddToBagButton from "../../../components/AddToBagButton";
-import {formatPrice} from '../../../utils/formatPrice'
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "../../../components/ProductCard";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export default function NewArrivals() {
-  const newArrivals = [
-    { title: "Pure Gold Necklace", image: img1, price: 67950.00 },
-    {
-      title: "Pure Handloom Gachi Tussar Saree",
-      image: img2,
-      price: 6990.00,
-    },
-    { title: "Plain Cotton Formal Shirt", image: img3, price: "2,950.00" },
-    {
-      title: "3pcs/set Minimalist Abstract Flower Vase",
-      image: img4,
-      price: 2499.00,
-    },
-  ];
+  const [data, setData] = useState(null);
+  const [isError, setError] = useState(false);
+  async function handleAddToCart(event, product) {
+    event.stopPropagation();
+    event.preventDefault();
+    //Add to cart logic
+  }
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(
+          `${BACKEND_URL}/products?sub_category=new-arrival&limit=4`
+        );
+        setData(response.data);
+        setError(null);
+      } catch (error) {
+        setData(null);
+        setError(error.response?.data.message || error.message);
+      }
+    }
+    getData();
+  }, []);
   return (
     <section className="bg-secondary">
       <p className="text-[7.25rem] font-light text-headings tracking-[4rem] w-full text-center">
@@ -32,30 +39,21 @@ export default function NewArrivals() {
           <img src={diamondBullet} alt="" />
           <p>New Arrivals</p>
         </div>
-        <ArrowButton style={2} text="Explore All" />
+        <NavLink to="/category/new-arrival">
+          <ArrowButton style={2} text="Explore All" />
+        </NavLink>
       </div>
-      <div className="grid grid-cols-4 gap-4 p-20 pt-0 h-[75vh]">
-        {newArrivals.map((item,index)=>(
-          <div key={index} className=" group h-full flex flex-col gap-3 overflow-hidden">
-          <div className="relative h-[85%] overflow-hidden">
-            <img
-              src={item.image}
-              alt=""
-              loading="lazy"
-              className="h-full w-full object-cover group-hover:scale-115 transition-transform ease-in-out duration-300"
-            />
-            <div className="absolute w-full p-4 -bottom-20 group-hover:bottom-0 transition-all ease-in-out duration-300">
-            <AddToBagButton/>
-            </div>
-          </div>
-          <div className="h-[15%] flex flex-col">
-            <p>{item.title}</p>
-            <p className="text-slate-500">₹ {formatPrice(item.price)}</p>
-          </div>
+      {isError ? (
+        <div className="flex items-center justify-center text-primary text-2xl min-h-[200px]">
+          {isError}
         </div>
-        ))}
-
-      </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-4 p-20 pt-0 h-[75vh] ">
+          {(data ? data : Array.from({ length: 4 })).map((product, index) => (
+            <ProductCard product={product} key={index} />
+          ))}
+        </div>
+      )}
     </section>
   );
 }
