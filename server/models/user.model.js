157db1ff6cac7d524c6674f3e962876b8903c909
Söùrãv <souravpramanick1997@gmail.com>
2025-05-02 11:01:45 +0530

    User model added

---
+++ server/models/user.model.js
@@ -0,0 +1,11 @@
+const pool = require("../config/postgres.config");
+
+module.exports = async function createUsersTable() {
+  const query = `CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    firebase_uid TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`;
+  await pool.query(query);
+};
