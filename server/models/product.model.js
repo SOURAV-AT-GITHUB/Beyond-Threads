45b32544dc501f0e13f9481f1d97865f96630a7c
Söùrãv <souravpramanick1997@gmail.com>
2025-05-02 11:02:21 +0530

    Products model added

---
+++ server/models/product.model.js
@@ -0,0 +1,28 @@
+const pool = require("../config/postgres.config");
+
+module.exports = async function createProductTable() {
+  const query = `CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    stock INTEGER NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    color_name TEXT NOT NULL,
+    color_codes TEXT[] NOT NULL,
+    pattern TEXT NOT NULL,
+    care TEXT NOT NULL,
+    category TEXT NOT NULL, 
+    sub_category TEXT NOT NULL,
+    product_type TEXT NOT NULL,
+    length TEXT NOT NULL,
+    blouse_piece TEXT NOT NULL,
+    fabric TEXT,
+    blouse TEXT,
+    disclaimer TEXT NOT NULL,
+    sku TEXT NOT NULL,
+    what_you_will_receive TEXT NOT NULL,
+    description TEXT NOT NULL,
+    note TEXT NOT NULL,
+    images TEXT[] NOT NULL
+        )`;
+  await pool.query(query);
+}
