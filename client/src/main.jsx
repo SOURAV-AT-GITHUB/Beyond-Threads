6f061697bd394ffdeef8ea70c7fbedc0dc3a5eb2
Söùrãv <souravpramanick1997@gmail.com>
2025-05-02 12:01:01 +0530

    redux and firebase added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./Store/store.js";
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </BrowserRouter>
+);
