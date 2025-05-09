5d0bd930e0f7dddf0f54f1445af92a7227209589
Söùrãv <souravpramanick1997@gmail.com>
2025-05-09 14:21:34 +0530

    User name field added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,7 +5,7 @@ module.exports = async function createUsersTable() {
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     firebase_uid TEXT UNIQUE NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
   await pool.query(query);
