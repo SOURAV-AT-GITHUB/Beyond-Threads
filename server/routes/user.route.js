75ff92777d13e6c8b387e2682703f87c0df2c93d
Söùrãv <souravpramanick1997@gmail.com>
2025-05-09 13:59:37 +0530

    Client login route updated and nested with cart route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,8 @@ const UserRoute = require("express").Router();
 const pool = require("../config/postgres.config");
 require("dotenv").config();
 const admin = require("../config/firebase.config");
+const CartRoute = require("./cart.route");
+UserRoute.use("/cart", CartRoute);
 
 UserRoute.post("/login", async (req, res) => {
   const { idToken } = req.body;
@@ -12,21 +14,36 @@ UserRoute.post("/login", async (req, res) => {
     const userCheck = await pool.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
-    let cartItems = []
+    let cartItems = [];
     if (userCheck.rowCount === 0) {
       await pool.query(
         "INSERT INTO users (email,firebase_uid) VALUES ($1, $2)",
         [email, uid]
       );
-    }else{
+    } else {
       const userId = userCheck.rows[0].id;
-      const cartResult = await pool.query("SELECT * FROM cart_items WHERE user_id = $1", [userId]);
+      const cartResult = await pool.query(
+        `
+  SELECT
+    cart_item.id AS id,
+    cart_item.quantity,
+    products.id AS product_id,
+    products.name,
+    products.images[1] AS image,
+    products.price,
+    products.stock
+  FROM cart_item
+  JOIN products ON cart_item.product_id = products.id
+  WHERE cart_item.user_id = $1
+`,
+        [userId]
+      );
       cartItems = cartResult.rows;
     }
-    res.json({token:idToken,cart:cartItems})
+    res.json({ token: idToken, cart: cartItems });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
 
-module.exports = UserRoute
+module.exports = UserRoute;
