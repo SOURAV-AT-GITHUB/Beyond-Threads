d922693446edca08ac531328e4c5100afb1e9fc7
Söùrãv <souravpramanick1997@gmail.com>
2025-05-11 16:34:50 +0530

    Loading/progress added on Add to bag button

--- client/src/components/AddToBagButton.jsx
+++ client/src/components/AddToBagButton.jsx
@@ -1,11 +1,12 @@
+import { CircularProgress } from "@mui/material";
 import blackCart from "/Images/cart-black.svg";
 import redCart from "/Images/cart-red.svg";
-export default function AddToBagButton() {
+export default function AddToBagButton({ isLoading = false }) {
   return (
     <div className="flex w-full h-[50px] border border-primary group/cart-button">
-      <p className="h-full w-3/4 bg-primary text-white flex items-center justify-center px-4">
-        Add To Bag
-      </p>
+      <div className="h-full w-3/4 bg-primary text-white flex items-center justify-center px-4">
+        {isLoading ? <CircularProgress sx={{color:"white"}}/> : <p>Add To Bag</p>}
+      </div>
       <div className="w-1/4 overflow-hidden h-full">
         <div className="w-[200%] bg-white flex -translate-x-2/4 group-hover/cart-button:-translate-x-0 transition-transform ease-in-out duration-300">
           <div className="px-2 w-full h-[50px] p-3 flex justify-center">
