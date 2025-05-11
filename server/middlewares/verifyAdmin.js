9fb3823b811af9859f0d85de4acba69e0aaff172
Söùrãv <souravpramanick1997@gmail.com>
2025-05-11 16:32:16 +0530

    admin verification middleware added

---
+++ server/middlewares/verifyAdmin.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY_ADMIN = process.env.JWT_SECRET_KEY_ADMIN;
+
+module.exports = function verifyJWT(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(" ")[0] || null;
+    if (!token) return res.status(401).json({ message: "Please Login First." });
+
+    jwt.verify(token, JWT_SECRET_KEY_ADMIN, (err, decoded) => {
+      if (err)
+        return res
+          .status(401)
+          .json({ message: "Token expired, please login again" });
+      req.user = decoded
+      return next();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error while authentication." });
+  }
+};
