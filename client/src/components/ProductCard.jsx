62f9bb36dd6474a34c0f3d0b4775941a062032f5
Söùrãv <souravpramanick1997@gmail.com>
2025-05-11 16:35:50 +0530

    Add to bag functionality added

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,20 +1,45 @@
-import React, { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import { Fragment, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { formatPrice } from "../utils/formatPrice";
 import AddToBagButton from "./AddToBagButton";
 import { Skeleton } from "@mui/material";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { ADD_OR_UPDATE_ITEM, USER_LOGOUT } from "../Store/actionTypes";
 export default function ProductCard({ product = null, color = null }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((store) => store.token);
+  const [isLoading, setIsLoading] = useState(false);
   async function handleAddToCart(event) {
     event.stopPropagation();
     event.preventDefault();
-    //Add to cart logic
+    if (!token) return navigate("/login");
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/client/cart`,
+        { product_id: product.id },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log(response);
+      dispatch({ type: ADD_OR_UPDATE_ITEM, payload: response.data });
+    } catch (error) {
+      if (error.status === 401) {
+        dispatch({ type: USER_LOGOUT });
+        navigate("/login");
+      }
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <NavLink to={product ? `/product/${product.id}` : null}>
       <div className=" group h-full min-h-[400px] max-h-[500px] flex flex-col gap-3 overflow-hidden">
         {product ? (
-          <div className="relative h-[85%] overflow-hidden">
+          <div className="relative h-[80%] overflow-hidden">
             <img
               src={product.images[0]}
               alt=""
@@ -23,19 +48,22 @@ export default function ProductCard({ product = null, color = null }) {
             />
             <button
               onClick={(e) => handleAddToCart(e, product)}
-              className="absolute w-full p-4 -bottom-20 group-hover:bottom-0 transition-all ease-in-out duration-300"
+              disabled={isLoading}
+              className="absolute w-full p-4 -bottom-20 group-hover:bottom-0 transition-all ease-in-out duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-pointer"
             >
-              <AddToBagButton />
+              <AddToBagButton isLoading={isLoading} />
             </button>
           </div>
         ) : (
           <Skeleton sx={{ height: "100%" }} />
         )}
-        <div className="h-[15%] flex flex-col">
+        <div className="h-[20%] flex flex-col">
           {product ? (
             <Fragment>
               <p
-                className={`${color === "white" ? "text-white" : "text-black"}`}
+                className={`${
+                  color === "white" ? "text-white" : "text-black"
+                } line-clamp-2`}
               >
                 {product.name}
               </p>
