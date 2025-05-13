daaff40cec85675b2aa7261a0e4a34bf39aa991b
Söùrãv <souravpramanick1997@gmail.com>
2025-05-13 22:20:54 +0530

    Sections navigation added(new arrivals only)

--- admin/src/components/SideNavbar.jsx
+++ admin/src/components/SideNavbar.jsx
@@ -7,12 +7,17 @@ import AcUnitIcon from "@mui/icons-material/AcUnit";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { NavLink, useLocation } from "react-router-dom";
 import { useState } from "react";
+import CategoryIcon from "@mui/icons-material/Category";
+import FiberNewIcon from "@mui/icons-material/FiberNew";
 export default function SideNavbar() {
   const { pathname } = useLocation();
   const [productsExpanded, setProductsExpanded] = useState(false);
   const toggleProducts = () => setProductsExpanded((prev) => !prev);
+  const [sectionsExpanded, setSectionsExpanded] = useState(false);
+  const toggleSections = () => setSectionsExpanded((prev) => !prev);
   return (
     <div className="sticky top-0 flex flex-col gap-1 border-r border-headings p-2">
+      
       <NavLink to="/">
         <div
           className={`flex items-center gap-2 p-2 text-lg ${
@@ -23,6 +28,7 @@ export default function SideNavbar() {
           <p>Dashboard</p>
         </div>
       </NavLink>
+
       <div
         className={`p-2 overflow-hidden ${
           productsExpanded ? "h-[200px]" : "h-[50px]"
@@ -53,7 +59,7 @@ export default function SideNavbar() {
               }`}
             >
               <ManIcon />
-              <p>Mensware</p>
+              <p>Menswear</p>
             </div>
           </NavLink>
           <NavLink to="/products/women">
@@ -65,7 +71,7 @@ export default function SideNavbar() {
               }`}
             >
               <WomanIcon />
-              <p>Womansware</p>
+              <p>Womanswear</p>
             </div>
           </NavLink>
           <NavLink to="/products/decor">
@@ -82,6 +88,7 @@ export default function SideNavbar() {
           </NavLink>
         </div>
       </div>
+
       <NavLink to="/orders">
         <div
           className={`flex items-center gap-2 p-2 text-lg ${
@@ -92,8 +99,42 @@ export default function SideNavbar() {
           <p>Orders</p>
         </div>
       </NavLink>
-      {/* <div className="flex">lorem</div>
-      <div className="flex">lorem</div> */}
+
+      <div
+        className={`p-2 overflow-hidden ${
+          sectionsExpanded ? "h-[200px]" : "h-[50px]"
+        } ${
+          pathname.includes("/sections/") && "text-primary"
+        } cursor-pointer transition-[height] duration-300 ease-in-out`}
+      >
+        <div
+          onClick={toggleSections}
+          className="flex justify-between items-center"
+        >
+          <div className="flex gap-2">
+            <CategoryIcon />
+            <p>Sections</p>
+          </div>
+          <KeyboardArrowDownIcon
+            className={`${sectionsExpanded ? "rotate-180" : ""}`}
+          />
+        </div>
+        {/**expand */}
+        <div className="flex flex-col gap-2 p-3">
+          <NavLink to="/sections/new-arrivals">
+            <div
+              className={`flex items-center gap-1 py-2 ${
+                pathname === "/sections/new-arrivals"
+                  ? "text-primary border-b"
+                  : "text-black"
+              }`}
+            >
+              <FiberNewIcon />
+              <p>New Arrivals</p>
+            </div>
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
