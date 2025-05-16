2a019be30199cffbca58a776ae18396794e7fe61
Söùrãv <souravpramanick1997@gmail.com>
2025-05-16 17:35:28 +0530

    Loading animation added on ArrowButton

--- client/src/components/ArrowButton.jsx
+++ client/src/components/ArrowButton.jsx
@@ -1,19 +1,24 @@
 import rightArrowWhite from "/Images/right-arrow-white.svg";
 import rightArrowBlack from "/Images/right-arrow-black.svg";
-export default function ArrowButton({ style = 1, text = "Default" }) {
+import CircularProgress from "@mui/material/CircularProgress";
+export default function ArrowButton({
+  style = 1,
+  text = "Default",
+  isLoading = false,
+}) {
   return (
     <div
       className={`flex items-center h-[50px] w-auto group/arrow-button border ${
         style === 1 ? "border-white" : "border-primary"
       }`}
     >
-      <p
+      <div
         className={`${
           style === 1 ? "bg-white text-black" : "bg-primary text-white"
         } text-black px-4 h-full w-full flex items-center`}
       >
-        {text}
-      </p>
+        {isLoading ? <CircularProgress color="" sx={{margin:"auto"}}/> : <p>{text}</p>}
+      </div>
       <div className=" w-[50px] overflow-hidden h-full ">
         <div className="w-[200%] flex -translate-x-2/4 group-hover/arrow-button:-translate-x-0 transition-transform ease-in-out duration-300">
           <div
