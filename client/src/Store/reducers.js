dbebf5e9e278ad208da8f619f19ddc7184afbdd5
Söùrãv <souravpramanick1997@gmail.com>
2025-05-19 23:46:44 +0530

    active coupons added on cartReducer with coupons management

--- client/src/Store/reducers.js
+++ client/src/Store/reducers.js
@@ -11,6 +11,7 @@ import {
   ORDERS_DATA_REQUEST,
   ORDERS_DATA_SUCCESS,
   ORDERS_DATA_ERROR,
+  ADD_FIRST200_COUPON,
 } from "./actionTypes";
 const initialAuthState = {
   user: null,
@@ -32,27 +33,77 @@ export function authReducer(state = initialAuthState, { type, payload }) {
   }
 }
 
-function getFinalPrice(products) {
-  if (products.length === 0) return 0.0;
-  return products
-    .reduce((total, product) => total + product.price * product.quantity, 0)
-    .toFixed(2);
-}
 const initialCartState = {
   isCartLoading: false,
   cartProducts: [],
   finalPrice: 0.0,
+  subTotal: 0.0,
   isCartError: null,
+  activeCoupons: [],
 };
+const coupons = [
+  {
+    code: "FIRST200",
+    type: "flat",
+    forFirstOrder: true,
+    amount: 200.0,
+    min_cart_value: 0.0,
+    max_cart_value: null,
+  },
+  {
+    code: "SAVE300",
+    type: "flat",
+    forFirstOrder: false,
+    amount: 300.0,
+    min_cart_value: 5000.0,
+    max_cart_value: 6999.0,
+  },
+  {
+    code: "SAVE500",
+    type: "flat",
+    forFirstOrder: false,
+    amount: 500.0,
+    min_cart_value: 7000.0,
+    max_cart_value: null,
+  },
+];
+function getSubTotal(products) {
+  if (products.length === 0) return 0.0;
+  return parseFloat(
+    products
+      .reduce((total, product) => total + product.price * product.quantity, 0)
+      .toFixed(2)
+  );
+}
+function updateCoupons(subTotal, activeCoupons) {
+  const result = activeCoupons.filter((coupon) => coupon.code === "FIRST200");
+  if (subTotal >= 5000 && subTotal <= 6999) {
+    result.push(coupons[1]);
+  } else if (subTotal >= 7000) {
+    result.push(coupons[2]);
+  }
+  return result;
+}
 export function cartReducer(state = initialCartState, { type, payload }) {
   switch (type) {
     case CART_DATA_REQUEST:
-      return { ...initialCartState, isCartLoading: true };
+      return { ...state, isCartLoading: true };
     case CART_DATA_SUCCESS: {
+      const subTotal = getSubTotal(payload);
+      const activeCoupons = updateCoupons(subTotal, state.activeCoupons);
+      const finalPrice = parseFloat(
+        activeCoupons
+          .reduce((acc, coupon) => acc - coupon.amount, subTotal)
+          .toFixed(2)
+      );
       return {
-        ...initialCartState,
+        ...state,
+        isCartLoading: false,
+        isCartError: null,
         cartProducts: payload,
-        finalPrice: getFinalPrice(payload),
+        finalPrice,
+        subTotal,
+        activeCoupons,
       };
     }
     case ADD_OR_UPDATE_ITEM: {
@@ -60,28 +111,62 @@ export function cartReducer(state = initialCartState, { type, payload }) {
         const updatedCartProducts = state.cartProducts.map((item) =>
           item.id === payload.id ? payload : item
         );
+        const subTotal = getSubTotal(updatedCartProducts);
+        const activeCoupons = updateCoupons(subTotal, state.activeCoupons);
+        const finalPrice = parseFloat(
+          activeCoupons
+            .reduce((acc, coupon) => acc - coupon.amount, subTotal)
+            .toFixed(2)
+        );
         return {
-          ...initialCartState,
+          ...state,
+          isCartLoading: false,
           cartProducts: updatedCartProducts,
-          finalPrice: getFinalPrice(updatedCartProducts),
+          finalPrice,
+          subTotal,
+          isCartError: null,
+          activeCoupons,
         };
       }
+      const updatedCartProducts = [...state.cartProducts, payload];
+      const subTotal = getSubTotal(updatedCartProducts);
+      const activeCoupons = updateCoupons(subTotal, state.activeCoupons);
+      const finalPrice = parseFloat(
+        activeCoupons
+          .reduce((acc, coupon) => acc - coupon.amount, subTotal)
+          .toFixed(2)
+      );
       return {
-        ...initialCartState,
-        cartProducts: [...state.cartProducts, payload],
-        finalPrice: getFinalPrice([...state.cartProducts, payload]),
+        ...state,
+        isCartLoading: false,
+        cartProducts: updatedCartProducts,
+        finalPrice,
+        subTotal,
+        isCartError: null,
+        activeCoupons,
       };
     }
     case REMOVE_SINGLE_ITEM: {
       const updatedCartProducts = state.cartProducts.filter(
         (item) => item.id !== payload.id
       );
+      const subTotal = getSubTotal(updatedCartProducts);
+      const activeCoupons = updateCoupons(subTotal, state.activeCoupons);
+      const finalPrice = activeCoupons
+        .reduce((acc, coupon) => acc - coupon.amount, subTotal)
+        .toFixed(2);
       return {
-        ...initialCartState,
+        ...state,
+        isCartLoading: false,
         cartProducts: updatedCartProducts,
-        finalPrice: getFinalPrice(updatedCartProducts),
+        finalPrice,
+        subTotal,
+        isCartError: null,
+        activeCoupons,
       };
     }
+    case ADD_FIRST200_COUPON:
+      return { ...state, activeCoupons: [...state.activeCoupons, coupons[0]] };
     case CART_DATA_ERROR:
       return { ...initialCartState, isCartError: payload };
     default:
@@ -108,4 +193,3 @@ export function ordersReducer(state = initialOrdersState, { type, payload }) {
       return state;
   }
 }
-
